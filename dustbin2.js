7920e68e94912e2f9e851a256fe0c97350f18fc1
raghavthecoder <65603739+raghavthecoder@users.noreply.github.com>
2020-06-25 22:05:32 +0530

    Add files via upload

---
+++ dustbin2.js
@@ -0,0 +1,11 @@
+class dustbin2{
+    constructor(x,y,width,height){
+        var options={
+            isStatic=true,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2,
+
+        }
+    }
+}
